57801a8d5f2f38a4cca445bf460dbe093b587c6c
userseva404 <userseva404@gmail.com>
2026-08-13 19:13:25 +0300

    proxy fix

--- src/lib/supabase/proxy.ts
+++ src/lib/supabase/proxy.ts
@@ -44,6 +44,11 @@ export async function updateSession(request: NextRequest) {
 
   const user = data?.claims;
 
+  if (request.nextUrl.pathname.includes("widget")) {
+    const url = request.nextUrl.clone();
+    return NextResponse.redirect(url);
+  }
+
   if (!user && !request.nextUrl.pathname.startsWith("/auth")) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();

2c20a031e939750d270ad177c41c743a64322077
userseva404 <userseva404@gmail.com>
2026-08-13 20:09:59 +0300

    img fix 1000%

--- src/utils/toBase64.ts
+++ src/utils/toBase64.ts
@@ -1,6 +1,14 @@
 export async function toBase64(url: string): Promise<string> {
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+      },
+    });
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }

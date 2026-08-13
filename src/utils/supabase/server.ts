1b178403accc038990433180bae85713ae769342
userseva404 <userseva404@gmail.com>
2026-08-13 19:20:44 +0300

    anon key update

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -7,7 +7,7 @@ export async function createClientServer() {
 
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {

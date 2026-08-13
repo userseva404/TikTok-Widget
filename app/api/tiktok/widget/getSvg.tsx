0fc2eb4e7c3324d2fd60190b2d7852054e9e0484
userseva404 <userseva404@gmail.com>
2026-08-13 19:06:37 +0300

    widget test

--- app/api/tiktok/widget/getSvg.tsx
+++ app/api/tiktok/widget/getSvg.tsx
@@ -3,13 +3,14 @@ import { Widget } from "@/components/Widget";
 import { IWidget } from "../data/route";
 import { Svg } from "@/components/Svg";
 import { readFileSync } from "fs";
+import path from "path";
+
+const cssPath = path.join(process.cwd(), "src", "style", "index.css");
 
 export async function getSvg(widget: IWidget) {
   const { renderToStaticMarkup } = await import("react-dom/server");
-  const styles = readFileSync(
-    "S:/Projects/tiktok-widget/src/style/index.css",
-    "utf8",
-  );
+
+  const styles = readFileSync(cssPath, "utf8");
 
   const svg = renderToStaticMarkup(
     <Svg params={{}} styles={styles}>

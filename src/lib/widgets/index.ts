0a6bde0b65b90e8fdd5181a3ce9b64b853db1a38
userseva404 <userseva404@gmail.com>
2026-08-16 19:54:51 +0300

    vercel deploy

--- src/lib/widgets/index.ts
+++ src/lib/widgets/index.ts
@@ -14,10 +14,10 @@ export const widgetsApi: IWidgetApi = {
   tiktok: {
     widgetDataApi: DEVELOP
       ? "https://d6xfx6ln-3000.euw.devtunnels.ms/api/tiktok/data/"
-      : "https://chimerical-stroopwafel-101dd2.netlify.app/api/tiktok/data/", // hard code
+      : "https://tik-tok-widget-three.vercel.app/api/tiktok/data/", // hard code
     widgetApi: DEVELOP
       ? "https://d6xfx6ln-3000.euw.devtunnels.ms/api/tiktok/widget/"
-      : "https://chimerical-stroopwafel-101dd2.netlify.app/api/tiktok/widget/", // hard code
+      : "https://tik-tok-widget-three.vercel.app/api/tiktok/widget/", // hard code
     supabaseProviderName: "tik_tok",
     name: "Tik Tok",
   },

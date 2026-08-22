e19715aa73721dcce2ee737557db9047588bbeba
userseva404 <userseva404@gmail.com>
2026-08-22 15:30:47 +0300

    add Vitest, unit tests for calcExpire incl. boundary cases

---
+++ src/utils/calcExpire.test.ts
@@ -0,0 +1,29 @@
+import { afterEach, describe, expect, test, vi } from "vitest";
+import { calcExpire } from "./calcExpire";
+
+describe("calcExpire", () => {
+  test("returns an ISO string N seconds in the future", () => {
+    const startPoint = 100;
+    const date = new Date(startPoint);
+    vi.setSystemTime(date);
+    expect(calcExpire(86400)).toBe(
+      new Date(startPoint + 86400 * 1000).toISOString(),
+    );
+  });
+  test("throws when expires_in is negative", () => {
+    const startPoint = 100;
+    const date = new Date(startPoint);
+    vi.setSystemTime(date);
+    expect(() => calcExpire(-10)).toThrow(Error);
+  });
+  test("returns current time when expires_in is 0
+", () => {
+    const startPoint = 100;
+    const date = new Date(startPoint);
+    vi.setSystemTime(date);
+    expect(calcExpire(0)).toBe(new Date(startPoint + 0 * 1000).toISOString());
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+});
--- src/utils/calcExpire.ts
+++ src/utils/calcExpire.ts
@@ -1,5 +1,9 @@
 export const calcExpire = (expires_in: number): string => {
+  if (expires_in < 0) {
+    throw new Error("Number cannot be less then 0");
+  }
+
   const invalid_at_ms = Date.now() + expires_in * 1000;
   const invalid_at = new Date(invalid_at_ms).toISOString();
-  return invalid_at
+  return invalid_at;
 };
